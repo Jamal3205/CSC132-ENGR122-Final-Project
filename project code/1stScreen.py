c5f4320ad42c03ab12d443f5307c09ca103973df
Jamal3205 <127755916+Jamal3205@users.noreply.github.com>
2023-04-17 10:39:10 -0500

    1stScreen.py
    starter screen for GUI

---
+++ project code/1stScreen.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+window = Tk()
+window.title("Pick Set Time or Set Weight")
+
+button_weight = Button(window, text= "Set Weight", width = 30, height = 20)
+button_weight.pack(side = LEFT, padx=20, pady=20)
+
+button_time = Button(window, text = "Set Time", width = 30, height = 20)
+button_time.pack(side = RIGHT, padx = 20, pady = 20)
+
+# start the tkinter event loop
+window.mainloop()
